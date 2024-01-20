4b21f21ea0d57961af2ecc539bd564be809176e6
ankit bhawsar <ankitbhawsar206@gmail.com>
2024-01-20 17:46:20 +0530

    intial commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,25 +4,24 @@ import ChatContainer from "@/components/ChatContainer";
 import SideBar from "@/components/SideBar";
 import { useAuthContext } from "@/context/AuthContext";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
   const router = useRouter();
-  const { currentUser }:any = useAuthContext();
-
-  const ProtectedRoute = ({ children }:any) => {
+  const { currentUser }: any = useAuthContext();
+  useEffect(() => {
     if (!currentUser) {
-      return router.push("/login");
+      router.push("/login");
     }
-    return children;
-  };
+  }, [currentUser]);
   return (
-    <ProtectedRoute>
+    <>
       <main className="h-screen flex justify-center items-center ">
         <div className="flex w-[60vw] h-[80vh] rounded-md overflow-hidden ">
           <SideBar />
           <ChatContainer />
         </div>
       </main>
-    </ProtectedRoute>
+    </>
   );
 }
